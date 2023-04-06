27be64f733da9e18d425abfbd49373fa55446be4
Driijko <driijko@gmail.com>
2023-04-06 14:53:43 -0400

    Splash: transfer code from previous version and simplify id names.

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,10 @@
   import currentPageName from "./stores/currentPageName";
   import setUp from "./helpers/setUp";
   import resetCount from "./stores/resetCount";
+  import Audio from "./components/Audio.svelte";
   import LoadingScreen from "./components/layers/LoadingScreen.svelte";
   import OpeningPrompt from "./components/layers/opening-prompt/OpeningPrompt.svelte";
-  import Audio from "./components/Audio.svelte";
+  import Splash from "./components/layers/splash/Splash.svelte";
 
   setUp();
 </script>
@@ -19,5 +20,9 @@
     <LoadingScreen />
   {:else if $currentPageName === "opening-prompt"}
     <OpeningPrompt />
+  {:else}
+    {#if $currentPageName === "splash"}
+      <Splash />
+    {/if}
   {/if}
 {/key}
--- src/helpers/siteSettings.js
+++ src/helpers/siteSettings.js
@@ -2,7 +2,7 @@ const siteSettings = {
   portraitOrientationUARRRatio: [9,16],
   landscapeOrientationUARRRatio: [16,9],
   resizeDelay: 2000,
-  startingPageName: "loading-screen",
+  startingPageName: "splash",
 }
 
 export default siteSettings;
